d5ce0af565e2df45cb830b28f90a9bbcc5f8a357
Korrat <korrat@protonmail.com>
2024-04-20 18:42:17 +0200

    feat: use notmuch library to discover config and filter path
    Previously, notcoal would use hardcoded paths to the notmuch
    configuration and the filter rules file, alongside notmuch hooks.
    However, notmuch offers number of fallback paths for the configuration
    file and hooks directory. In some scenarios, notcoal could not find
    notmuch configuration automatically, and users would need to specify the
    path to the configuration file manually.
    
    With this patch, notcoal will use the notmuch library bindings to open
    a notmuch database. Any interaction with the notmuch configuration then
    goes through the database. With this approach, notcoal discovers the
    notmuch database and configuration file following the same logic as
    notmuch itself. Similarly, the path to the filter rules file is based on
    the path to notmuch's hooks directory.
    
    Fixes #7.

--- src/bin/notcoal.rs
+++ src/bin/notcoal.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use ini::Ini;
 use notcoal::*;
+use notmuch::{ConfigKey, Database, DatabaseMode};
 use std::path::{Path, PathBuf};
 use std::process;
 
@@ -20,59 +20,40 @@ struct Opt {
     /// Leave the "query tag" in place instead of removing once all filters ran
     leave: bool,
     #[arg(long = "sync-flags")]
-    /// Force maildir flag syncing  (overrides setting found in config) [true |
-    /// false]
+    /// Force maildir flag syncing  (overrides setting found in config)
     flags: Option<bool>,
     #[arg(long = "dry-run")]
     dry: bool,
 }
 
-pub fn get_config(config: &Option<PathBuf>) -> Ini {
-    let mut p: PathBuf;
-    let config = match config {
-        Some(p) => p,
-        None => {
-            p = match dirs::home_dir() {
-                Some(h) => h,
-                None => {
-                    eprintln!("Cannot determine home directory, aborting");
-                    process::exit(1);
-                }
-            };
-            p.push(".notmuch-config");
-            &p
-        }
-    };
-    match Ini::load_from_file(config) {
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!("{}\nDo you have notmuch configured?", e);
+pub fn get_maildir_sync_db(db: &Database) -> bool {
+    match db.config_bool(ConfigKey::MaildirFlags) {
+        Ok(bool) => bool,
+        Err(err) => {
+            eprintln!("Could not open notmuch database, aborting!");
+            eprintln!("Error: {err}");
             process::exit(1);
         }
     }
 }
 
-pub fn get_maildir_sync(config: &Ini) -> bool {
-    matches!(
-        config.get_from(Some("maildir"), "synchronize_flags"),
-        Some("true")
-    )
-}
-
-pub fn get_db_path(config: &Ini) -> Option<PathBuf> {
-    Some(PathBuf::from(config.get_from(Some("database"), "path")?))
-}
-
-pub fn get_filters(path: &Option<PathBuf>, db_path: &Path) -> Vec<Filter> {
+pub fn get_filters(path: &Option<PathBuf>, db: &Database) -> Vec<Filter> {
     let mut p: PathBuf;
     let filter_path = match path {
         Some(p) => p,
         None => {
-            p = db_path.to_path_buf();
-            p.push(".notmuch/hooks/notcoal-rules.json");
+            p = match db.config(ConfigKey::HookDir) {
+                Some(path) => PathBuf::from(path),
+                None => {
+                    eprintln!("Could not determine notmuch hooks directory, aborting!");
+                    process::exit(1);
+                }
+            };
+            p.push("notcoal-rules.json");
             &p
         }
     };
+
     match filters_from_file(filter_path) {
         Ok(f) => f,
         Err(e) => {
@@ -85,11 +66,22 @@ pub fn get_filters(path: &Option<PathBuf>, db_path: &Path) -> Vec<Filter> {
 
 fn main() {
     let opt = Opt::parse();
-    let config = get_config(&opt.config);
-    let db_path = match get_db_path(&config) {
-        Some(db) => db,
-        None => {
-            eprintln!("Can't get the database path");
+
+    let db = match Database::open_with_config::<&Path, _>(
+        None,
+        if opt.dry {
+            DatabaseMode::ReadOnly
+        } else {
+            DatabaseMode::ReadWrite
+        },
+        opt.config,
+        None,
+    ) {
+        Ok(db) => db,
+        Err(err) => {
+            eprintln!("Could not open notmuch database, aborting!");
+            eprintln!("Error: {err}");
+            eprintln!("Do you have notmuch configured?");
             process::exit(1);
         }
     };
@@ -97,14 +89,14 @@ fn main() {
     let options = FilterOptions {
         sync_tags: match &opt.flags {
             Some(b) => *b,
-            None => get_maildir_sync(&config),
+            None => get_maildir_sync_db(&db),
         },
         leave_tag: opt.leave,
     };
-    let filters = get_filters(&opt.filters, &db_path);
+    let filters = get_filters(&opt.filters, &db);
 
     if opt.dry {
-        match filter_dry_with_path::<PathBuf, PathBuf>(&db_path, &opt.tag, &filters) {
+        match filter_dry(&db, &opt.tag, &filters) {
             Ok(m) => {
                 println!("There are {} matches:", m.0);
                 for info in m.1 {
@@ -119,7 +111,7 @@ fn main() {
         process::exit(0);
     }
 
-    match filter_with_path::<PathBuf, PathBuf>(&db_path, &opt.tag, &options, &filters) {
+    match filter(&db, &opt.tag, &options, &filters) {
         Ok(m) => {
             if m > 0 {
                 println!("Yay you successfully applied {} filters", m);
